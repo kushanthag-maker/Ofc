7803f30e294e336988230c3158c0f8baa6798dae
kushanthag-maker <kushanthag@gmail.com>
2026-08-02 20:22:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-app.use('/code', code);
-app.get('/update-config', (req, res) => {
-    res.send("Update config route working ✅");
-});
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
-});
+let code = require('./pair');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/code', code);
+
+app.get('/update-config', (req, res) => {
+    res.send("Update config route working ✅");
+});
+
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
 app.listen(PORT, () => {
-console.log(`
+    console.log(`
  ██████╗  ██╗  ██╗ ██████╗ ███████╗████████╗
 ██╔════╝  ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝
 ██║  ███╗ ███████║██║   ██║███████╗   ██║   
@@ -30,14 +34,12 @@ console.log(`
 ╚██████╔╝ ██║  ██║╚██████╔╝███████║   ██║   
  ╚═════╝  ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   
 
-👻 GHOST X MD SERVER ONLINE
+👻 NEXUS MD SERVER ONLINE
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-💖 Don't Forget To Give a Star
-⚡ Bot    : GHOST X MD MINI
+⚡ Bot    : NEXUS MD
 🌐 URL    : http://localhost:${PORT}
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-`)
-
+`);
 });
 
 module.exports = app;
